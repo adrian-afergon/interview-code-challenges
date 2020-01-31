9f1cdc991503d6382dfc8b1dc8a87649caef18bb
Adrian Ferrera <adrian.afergon@gmail.com>
2020-01-31 09:50:06 +0000

    Setup redux

---
+++ web/src/config/reducer.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import {DefaultRootState} from 'react-redux';
+
+export type RootApplicationState = {
+
+} & DefaultRootState;
+
+export default combineReducers({
+
+});
