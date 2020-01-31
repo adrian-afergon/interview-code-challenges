35eeb715622dc29be2890bd3de9aad8318fcccf1
Adrian Ferrera <adrian.afergon@gmail.com>
2020-01-31 18:00:09 +0000

    Change ports

--- web/src/.env/environment.ts
+++ web/src/.env/environment.ts
@@ -1,4 +1,4 @@
 export enum Constants {
-    API_URL = 'http://localhost:3000',
-    ASSETS_URL = 'http://localhost:3001/images'
+    API_URL = 'http://localhost:8080',
+    ASSETS_URL = 'http://localhost:3000/images'
 }
