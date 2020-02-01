63c34e55ee7bfcd9f8c72a01bb5bc1c0044b0c6d
Adrian Ferrera <adrian.afergon@gmail.com>
2020-02-01 22:53:16 +0000

    Remove irrelevant spinner test.

--- web/src/components/Spinner/Spinner.tsx
+++ web/src/components/Spinner/Spinner.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './Spinner.scss';
 
-export const Spinner: React.FC<{}> = () => (
+export const Spinner: React.FC = () => (
   <div className="Spinner">
     <div>
       <i className="icon-spin1 animate-spin" />
