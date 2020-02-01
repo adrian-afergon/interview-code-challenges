54a4fd60c9f0e38ad2ec4829b4b215c13ba5c79e
Adrian Ferrera <adrian.afergon@gmail.com>
2020-02-01 22:16:28 +0000

    Add roles to relevant elements in tests.

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import './Header.scss';
 import { NavLink } from 'react-router-dom';
 
-export const Header: React.FC<{}> = () => (
+export const Header: React.FC = () => (
   <header className="Header">
     <div className="container menu">
-      <NavLink to={'/'} activeClassName="is-active" exact={true}>
+      <NavLink to={'/'} activeClassName="is-active" exact={true} role="back">
         <i className="icon-left-open" />
         <h1>Phone Catalogue</h1>
       </NavLink>
--- web/src/components/PhoneItem/PhoneItem.tsx
+++ web/src/components/PhoneItem/PhoneItem.tsx
@@ -14,9 +14,9 @@ export const PhoneItem: React.FC<PhoneItemProps> = ({ phone }) => (
     <div className="item-image" style={{
         backgroundImage: `url(${Constants.ASSETS_URL}/${phone.imageFileName})`,
       }}/>
-    <h3>{phone.manufacturer}</h3>
-    <h2>{phone.name}</h2>
-    <div className="price"><span>{phone.price}</span> <span>€</span></div>
+    <h3 role="manufacturer">{phone.manufacturer}</h3>
+    <h2 role="name">{phone.name}</h2>
+    <div className="price" role="name"><span>{phone.price}</span> <span>€</span></div>
   </div>
 );
 
