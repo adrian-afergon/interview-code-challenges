d6135e0ec9989d7b9c4b8090867096f23bc72c44
Adrian Ferrera <adrian.afergon@gmail.com>
2020-02-01 16:52:02 +0000

    Add styles to the spinner components

--- web/src/components/Spinner/Spinner.tsx
+++ web/src/components/Spinner/Spinner.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import './Spinner.scss';
 
 export const Spinner: React.FC<{}> = () => (
-  <div className="Spinner">Hello from Spinner!</div>
+  <div className="Spinner">
+    <div>
+      <i className="icon-spin1 animate-spin" />
+    </div>
+  </div>
 );
 
 Spinner.displayName = 'Spinner';
