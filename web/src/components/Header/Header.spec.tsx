9e22a48370cbe2ed271a24d15a312ab558acf1c6
Adrian Ferrera <adrian.afergon@gmail.com>
2020-02-01 22:52:34 +0000

    Fix header default test

--- web/src/components/Header/Header.spec.tsx
+++ web/src/components/Header/Header.spec.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { render, RenderResult } from '@testing-library/react';
 import { Header} from './index';
+import { MemoryRouter } from 'react-router-dom';
+import { HeaderMessages } from './Header';
 
 describe('Header', () => {
   it('should display the default message', () => {
     const renderResult: RenderResult = render(
-      <Header/>,
+      <MemoryRouter>
+        <Header/>
+      </MemoryRouter>,
     );
-    expect(renderResult.queryByText('Hello from Header!')).toBeTruthy();
+    expect(renderResult.queryByText(HeaderMessages.TITLE)).toBeTruthy();
   });
 });
--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -2,12 +2,16 @@ import * as React from 'react';
 import './Header.scss';
 import { NavLink } from 'react-router-dom';
 
+export enum HeaderMessages {
+  TITLE = 'Phone Catalogue'
+}
+
 export const Header: React.FC = () => (
   <header className="Header">
     <div className="container menu">
       <NavLink to={'/'} activeClassName="is-active" exact={true} role="back">
         <i className="icon-left-open" />
-        <h1>Phone Catalogue</h1>
+        <h1>{HeaderMessages.TITLE}</h1>
       </NavLink>
     </div>
   </header>
